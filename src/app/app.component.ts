25d552b9582daa1f8397a4e2b601f40a719b7a36
sanjay-15 <sanjay.831719@gmail.com>
2021-10-02 15:55:06 +0530

    my second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,76 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+
+  email:String = "";
+  password:String = "";
+  userDetails:any = [];
+  user:any = {
+  firstName:"",
+  lastName:"",
+  email:"",
+  password:"",
+  confirmPassword:""
+  };
+
+  showRegistration:boolean = false;
+  showLogin:boolean = true;
+
+  openRegistration(){
+    this.user = {
+      firstName:"",
+      lastName:"",
+      email:"",
+      password:"",
+      confirmPassword:""
+      };
+
+      this.showRegistration = true;
+      this.showLogin = false;
+  }  
+
+  createAccount(){
+    if(this.user.firstName==""||this.user.lastName==""||this.user.email==""||this.user.password==""||this.user.confirmPassword==""){
+      alert("all fields are required");
+    }else{
+    if(this.user.password != this.user.confirmPassword){
+      alert("password and confirm password not matched");
+    }
+    else{
+      this.userDetails.push(this.user);
+      alert("Account was created");
+      this.showRegistration = false;
+      this.showLogin = true;
+    }
+   }
+  }
+
+  validate(){
+    let isEmailNotMatch = false;
+    let isPasswordNotMatch = false;
+
+    for(let i=0;i<this.userDetails.length;i++){
+      if(this.userDetails[i].email==this.email){
+        isEmailNotMatch = false;
+      if(this.userDetails[i].password==this.password){
+        isPasswordNotMatch = false;
+        alert("Logined Successfully");
+        break;
+      }
+      else{
+        isPasswordNotMatch = true;
+      }
+    }
+      else{
+        isEmailNotMatch = true;
+      }
+    }
+    if(isEmailNotMatch){
+      alert("Enter a valid email");
+    }
+    else if(isPasswordNotMatch){
+      alert("Enter a valid password");
+    }
+    
+  }
 }
